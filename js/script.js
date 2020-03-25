24c558c20adf532355ddd95ed93e8539b675b0e6
Steven Ly <1stevenly92@gmail.com>
2020-03-25 09:47:05 -0700

    finished form-letter

--- js/script.js
+++ js/script.js
@@ -1,10 +1,29 @@
-var bunnyName = "Flopsy";
+// var bunnyName = "Flopsy";
 
-function hippityHoppity(){
-    var bunnyName ="Mopsy"
-    alert(bunnyName);
-    bunnyName="Cottontail";
-}
+// function hippityHoppity(){
+//     var bunnyName ="Mopsy"
+//     alert(bunnyName);
+//     bunnyName="Cottontail";
+// }
 
-hippityHoppity();
-alert(bunnyName);
+// hippityHoppity();
+// alert(bunnyName);
+
+//form letter
+// $(document).ready(function(){
+
+//     var name = prompt("enter your name");
+
+//     console.log(name);
+
+//     function letter(name){
+//         prompt("Lorem ipsum dolor sit amet, consectetur adipiscing elit,  +sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. " + name )
+
+
+
+
+//     }
+//     letter(name);
+
+//})
+
